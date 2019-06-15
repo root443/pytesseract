9993a848397a87faa317e80b543c2f92a2d724b0
Sandro <s.conte@pharmawizard.com>
2019-06-15 02:07:27 +0200

    add Arab training

--- ocr.py
+++ ocr.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     ap = argparse.ArgumentParser()
     ap.add_argument("-i", "--image", required=True, help="path to input image to be OCR'd")
     ap.add_argument("-p", "--preprocess", type=str, default="thresh", help="type of preprocessing to be done")
-    ap.add_argument("--psm", type=str, default=3, help="page segmentation mode")
+    ap.add_argument("--psm", type=str, default=1, help="page segmentation mode")
     ap.add_argument("-o", "--out-name", type=str, default=datetime.now().strftime("%m-%d-%Y_%H-%M-%S"), help="file name")
 
     args = vars(ap.parse_args())
